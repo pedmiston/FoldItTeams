9c1179846ba4af351affdcd20e201244c1fd8478
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-08-04 16:05:19 -0500

    Refactoring to allow for parsing top pdb

--- scripts/parse_solution_paths.py
+++ scripts/parse_solution_paths.py
@@ -9,19 +9,28 @@ re_solution_path = re.compile(r"""
     solution_(?P<prefix>[a-z]+)_(?P<rank>\d+)_\d+_\d+_\d+.ir_solution.pdb
     """, re.X)
 
-if __name__ == '__main__':
-    solutions = pandas.read_csv('playbooks/data/puzzles_with_top_bid_solutions.txt',
-                                names=['path'])
+def read_solution_paths(solution_paths_txt):
+    solutions = pandas.read_csv(solution_paths_txt, names=['path'])
 
     # Extract data from path to solution data
     solution_path_data = solutions.path.str.extract(re_solution_path, expand=True)
+
+    # Convert extracted objects to numeric types
     solution_path_data['puzzle_id'] = pandas.to_numeric(solution_path_data.puzzle_id)
     solution_path_data['rank'] = pandas.to_numeric(solution_path_data['rank'])
 
+    # Merge paths with data
     solutions = (solutions.join(solution_path_data)
                           .sort_values(['puzzle_id', 'rank'])
                           .reset_index(drop=True))
 
+    # Rearrange columns to put the path str last
     output_cols = 'puzzle_id prefix rank path'.split()
     assert set(output_cols) == set(solutions.columns)
-    solutions[output_cols].to_csv('data/top_bid_solutions.csv', index=False)
+    solutions = solutions[output_cols]
+
+    return solutions
+
+if __name__ == '__main__':
+    solutions = read_solution_paths('data/puzzles_with_top_bid_solutions.txt')
+    solutions.to_csv('data/top_bid_solutions.csv', index=False)
---
+++ scripts/parse_solution_pdb_files.py
@@ -0,0 +1,11 @@
+import parse_solution_paths
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('puzzle_id')
+    args = parser.parse_args()
+
+
+    solutions = pandas.read_csv('data/top_bid_solutions.csv')
