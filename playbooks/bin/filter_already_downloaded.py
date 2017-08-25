ab89d483f457ae1c5e26adefd84b037477cf73c5
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-08-25 10:08:14 -0500

    Break up parsing into batches

--- playbooks/bin/filter_already_downloaded.py
+++ playbooks/bin/filter_already_downloaded.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
+import sys
+import glob
+import json
 import pandas
 import unipath
 
-PLAYBOOKS = '~/foldit/playbooks'
-TOP_DATA = unipath.Path(PLAYBOOKS, 'data/top')
+PLAYBOOKS = unipath.Path('~/foldit/playbooks')
+TOP_DATA = PLAYBOOKS + 'data/top'
+SOLUTION_DATA = TOP_DATA + '/run-*/solution_data.json'
 
-available_solutions = pandas.read_table(
-    unipath.Path(TOP_DATA, 'available_solutions.txt'),
-    names=['path']
-)
 
-downloaded_solutions_csv = unipath.Path(TOP_DATA, 'solution_data.csv')
+def extract_solution_filenames(solution_data):
+    solution_filenames = []
+    return solution_filenames
 
-if downloaded_solutions_csv.exists():
-    downloaded_solutions = pandas.read_csv(downloaded_solutions_csv)
-    downloaded_solutions = downloaded_solutions[['path']]
-    solutions_not_downloaded = available_solutions.path[
-        available_solutions.path.notin(downloaded_solutions.path)
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2 and unipath.Path(sys.argv[1]).exists()
+    available_solutions = pandas.read_table(sys.argv[1], names=['path']).path
+
+    already_downloaded = []
+    for solution_data in glob.glob(SOLUTION_DATA):
+        solution_filenames = extract_solution_filenames(solution_data)
+        already_downloaded.extend(solution_filename)
+
+    solutions_not_downloaded = available_solutions.ix[
+        ~available_solutions.isin(already_downloaded)
     ]
-else:
-    solutions_not_downloaded = available_solutions
-
-solutions_not_downloaded.path.to_csv(
-    unipath.Path(TOP_DATA, 'remaining_solutions.txt'),
-    index=False
-)
+    solutions_not_downloaded.to_csv(sys.stdout, index=False)
