59df5759ae39700262c0116f0aa8a43e53741173
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-08-30 16:04:15 -0500

    First stab at refactoring for new pipeline approach

--- main.go
+++ main.go
@@ -1,111 +1,57 @@
 /*foldit extracts data from FoldIt solution files.
 
-Subcommands:
-	foldit new [options]   # convert new solution files to json data
-	foldit tidy [options]  # convert json data to structured csvs
-
-
-# foldit new [options]
-
-The `new` subcommand processes new solution files in batches.
-
 Usage:
-	foldit new -input top_solution_file_paths.txt -type top -output top_solution_data.json
-	cat top_solution_file_paths.txt | foldit new -type top > top_solution_data.json
+	foldit \
+		-pdbs top_solution_file_paths.txt \
+		-pdbType top \
+		-outputDir top_solutions
 
-File paths can be provided on stdin or in a file. Output gets sent
-to the output file or stdout if none is given.
-
-There are two types of solution files:
-
-1. Regular solution files
-2. Top solution files
-
-Regular solution files are saved at regular intervals during gameplay. Top
-solution files are solutions that have been evaluated for quality compared to all
-other submissions. Top solution files contain data in the filename (e.g., the type
-of ranking and the numeric rank of this particular solution.).
-
-
-# foldit tidy [options]
-
-The `tidy` subcommand assembles csvs from all batched solution files. There
-are three types of tidy outputs that can be created from each solution struct.
-
-1. Scores
-2. Actions
-3. History
-
-Usage:
-	foldit tidy -input top_solution_data.json -type actions -output top_solution_actions.csv
+Args:
+	pdbs: Paths to solution files ending in a pdb extension. Paths can be provided
+		in a file or via stdin.
+	pdbType: Type of solution file. The two types of solution files are "regular"
+		and "top". A top solution is just a regular solution with ranking data
+		embedded in the name of the file.
+	outputDir: Where to write the data. Since each solution generates multiple
+		output files, the output must be a directory.
 */
 package main
 
 import (
+	"bufio"
 	"flag"
 	"log"
 	"os"
 )
 
 var (
-	solutionType, input, output *string
-	inputFile, outputFile       *os.File
-	err                         error
+	pdbType, pdbs, outputDir *string
+	scanner                  *bufio.Scanner
 )
 
 func main() {
-	solutionType = flag.String("type", "", "Type of solution file. Can be 'top' or 'regular'.")
-	input = flag.String("input", "", "Files to process. Defaults to Stdin.")
-	output = flag.String("output", "", "Destination for data. Defaults to Stdout.")
+	pdbs = flag.String("pdbs", "", "Files to process. Defaults to Stdin.")
+	pdbType = flag.String("pdbType", "", "Type of solution file. Can be 'top' or 'regular'.")
+	outputDir = flag.String("outputDir", "", "Destination for output files.")
 	flag.Parse()
 
-	if *input != "" {
-		inputFile, err = os.Open(*input)
+	var input *os.File
+	if *pdbs != "" {
+		input, err := os.Open(*pdbs)
 		if err != nil {
-			log.Fatalln("Problem opening input file")
+			log.Fatalln("Couldn't open input file")
 		}
 	} else {
-		inputFile = os.Stdin
+		input := os.Stdin
 	}
+	scanner := bufio.NewScanner(input)
 
-	if *output != "" {
-		outputFile, err = os.Create(*output)
-		if err != nil {
-			log.Fatalln("Error opening output file:", *output)
-		}
-	} else {
-		outputFile = os.Stdout
-	}
-
-	subcommand := flag.Args()[0]
-	switch subcommand {
-	case "new":
-		newSolutions(inputFile, outputFile)
-	case "tidy":
-		tidy(inputFile, outputFile)
-	default:
-		log.Fatalln("first arg " + subcommand + " must be 'new' or 'tidy'")
-	}
-}
-
-func newSolutions(input *os.File, output *os.File) {
-	switch *solutionType {
+	switch *pdbType {
 	case "top":
-		writeTopSolutionsToJSON(inputFile, outputFile)
+		extractTopSolutionData(scanner, *outputDir)
 	case "regular":
-		panic("Need to implement parseRegularSolution")
+		panic("Need to implement extractSolutionData")
 	default:
-		panic("unknown solutionType")
-	}
-}
-
-func tidy(input *os.File, output *os.File) {
-	switch *solutionType {
-	case "scores":
-		//
-	case "actions":
-		//
-	default:
-		panic("unknown solutionType")
+		panic("unknown pdbType")
 	}
 }
--- top_solution.go
+++ top_solution.go
@@ -1,8 +1,109 @@
 package main
 
+import (
+	"bufio"
+	"encoding/csv"
+	"errors"
+	"log"
+	"os"
+	"path"
+	"regexp"
+	"strconv"
+)
+
 // A TopSolution is a solution with ranking information
 type TopSolution struct {
 	Solution
 	RankType string
 	Rank     int
+}
+
+var (
+	reRankInfo = regexp.MustCompile(
+		`solution_(?P<RankType>[a-z]+)_(?P<Rank>\d+)_\d+_\d+_\d+.ir_solution.pdb`)
+)
+
+type tidyResult struct {
+	Scores  [][]string
+	Actions [][]string
+	History [][]string
+}
+
+func extractTopSolutionData(scanner *bufio.Scanner, outputDir string) {
+	topSolutioner := readAllTopSolutionFiles(scanner)
+	tidySolutioner := tidyAllTopSolutions(topSolutioner)
+
+	scoresWriter := createWriter(outputDir, "scores")
+	actionsWriter := createWriter(outputDir, "actions")
+	historyWriter := createWriter(outputDir, "history")
+
+	for tidyResult := range tidySolutioner {
+		for _, line := range tidyResult.Scores {
+			scoresWriter.Write(line)
+		}
+		for _, line := range tidyResult.Actions {
+			actionsWriter.Write(line)
+		}
+		for _, line := range tidyResult.History {
+			historyWriter.Write(line)
+		}
+	}
+}
+
+func readAllTopSolutionFiles(scanner *bufio.Scanner) <-chan *TopSolution {
+	out := make(chan *TopSolution)
+	go func() {
+		for scanner.Scan() {
+			out <- readTopSolutionFile(scanner.Text())
+		}
+		close(out)
+	}()
+	return out
+}
+
+func readTopSolutionFile(name string) *TopSolution {
+	solution, err := readSolution(name)
+	rankType, rank, _ := readRankFromFilename(name)
+	topSolution := &TopSolution{
+		Solution: solution,
+		RankType: rankType,
+		Rank:     rank,
+	}
+	return topSolution
+}
+
+func readRankFromFilename(name string) (rankType string, rank int, err error) {
+	matches := reRankInfo.FindAllStringSubmatch(name, -1)
+	if len(matches) == 0 {
+		err = errors.New("Unable to read rank info from filename: " + name)
+		return
+	}
+	matchValues := matches[0]
+	rankType = matchValues[1]
+	rank, err = strconv.Atoi(matchValues[2])
+	if err != nil {
+		err = errors.New("Unable to convert rank to integer: " + matchValues[2])
+	}
+	return
+}
+
+func tidyAllTopSolutions(in <-chan *TopSolution) <-chan tidyResult {
+	out := make(chan tidyResult)
+	go func() {
+		for topSolution := range in {
+			out <- tidyTopSolution(topSolution)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func createWriter(outputDir, dataType string) *csv.Writer {
+	outputDst := path.Join(outputDir, dataType+".csv")
+	outputFile, err := os.Open(outputDst)
+	if err != nil {
+		log.Fatalln("Can't open output file " + outputDst)
+	}
+	outputWriter := csv.NewWriter(outputFile)
+	return outputWriter
 }
